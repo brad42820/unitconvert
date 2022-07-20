f543b85f597a7451392620b848441b58e43097ab
Bradley Treece <btreece@mercyhurst.edu>
2022-07-20 14:37:51 -0400

    Changing the way that constants import

--- unitconvert/__init__.py
+++ unitconvert/__init__.py
@@ -1,12 +1,4 @@
-# from .constants import CONSTANTS
-# from .pressure import joke, print_kB
-# from .length import print_length_options, convert
-# from .area import print_area_options, convert
-# from .volume import print_volume_options, convert
-# from .time import print_time_options, convert
-# from .speed import print_speed_options, convert
-
-import unitconvert.constants
+import unitconvert.constants.CONSTANTS as unitconvert.CONSTANTS
 import unitconvert.length
 import unitconvert.area
 import unitconvert.volume
